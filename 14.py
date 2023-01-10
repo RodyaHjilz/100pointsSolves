eaa068be849bdbe29d789a839fb5e7e864b9fc3a
RodyaHjilz <113127693+RodyaHjilz@users.noreply.github.com>
2023-01-11 01:14:23 +0500

    Add files via upload

--- 14.py
+++ 14.py
@@ -1,7 +1,7 @@
 
 for a in range(1, 1000000):
 
-    if any(((int(f'2{y}23{x}5',15) + a)%int(f'67{x}9{y}', 13)) == 0 for x in range(13) for y in range(13)):
+    if any(((int(f'2{y}23{x}5',15) + a)%int(f'67{x}9{y}', 13)) == 0 for x in "0123456789ABC" for y in "0123456789ABC"):
         print(a)
         break
 
